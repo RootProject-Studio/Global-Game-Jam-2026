6b2bfa1c4397cb4fe78158ec2d41ef48e15d8ac0
BoboyyV2 <gabinboillon@gmail.com>
2026-01-30 23:49:15 +0100

    ajout d'une premiere map d'égout pas belle

---
+++ data/maps/sewer_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="sewer_tileset" tilewidth="20" tileheight="20" tilecount="336" columns="14">
+ <image source="../../assets/sewer_tileset/sewer_tileset.png" width="280" height="480"/>
+</tileset>
